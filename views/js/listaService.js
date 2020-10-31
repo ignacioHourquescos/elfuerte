32cf9643dec8582759d453e17c542b705bfedbc8
jimancio <ihourquescos@gmail.com>
2020-10-31 17:28:51 -0300

    lsitas con filtrso funcionando

--- views/js/listaService.js
+++ views/js/listaService.js
@@ -1,56 +1,38 @@
 
-// var mydata = [
-//     {
-//         "id": 0,
-//         "no": "test0",
-//         "ps": "$0",
-//         "pc": "$100",
-//         "or": "turquia"
-//     }
-// ];
 
 
-// document.getElementById("myTable").bootstrapTable({
-//     data: mydata
-// });
+document.getElementById("select").addEventListener("click", function() {
 
-
-
+    obtenerListaEspecifica();
+});
 
 
  function obtenerListaEspecifica () {
- 	$(document).ready($.getJSON('https://spreadsheets.google.com/feeds/list/1oEIH4yWEVMNmJOENvGau2Le9hGssFQss6RiFp7_EzFU/1/public/values?alt=json', function (data) {       
-  
+
+ 	$.getJSON('https://spreadsheets.google.com/feeds/list/1oEIH4yWEVMNmJOENvGau2Le9hGssFQss6RiFp7_EzFU/1/public/values?alt=json', function (data) {       
+        var categoria_filtrada=document.getElementById("select").value;
         var dataFiltrada=data.feed.entry;
-        console.log(dataFiltrada[0].gsx$categoria.$t);
-        console.log(dataFiltrada);
+        var dataFiltrada2;
+
+        if(categoria_filtrada=="Todas las Categorias"){
+            dataFiltrada2=dataFiltrada;
+        }else{
+            dataFiltrada2 = dataFiltrada.filter(function(producto) {
+                return producto.gsx$categoria.$t == categoria_filtrada;
+            });
+        }
+
+
+        $('table').bootstrapTable('destroy');
         $('table').bootstrapTable({ 
-            data: dataFiltrada,
+            data: dataFiltrada2,
         }); 
-    }));
-   
+        
+    });
+    
 }
+
+
  
  obtenerListaEspecifica();
 
-
-//  data.feed.entry[i].gsx$codigo.$t,
-//  data.feed.entry[i].gsx$descripcion.$t,
-//  data.feed.entry[i].gsx$precio.$t,
-//  data.feed.entry[i].gsx$presentacion.$t,
-//  data.feed.entry[i].gsx$origen.$t,  
-
-
-// $('#filterBy').click(function () {
-//     var filterAlgorithm = $('[name="filterAlgorithm"]').val()
-
-//     $table.bootstrapTable('refreshOptions', {
-//       filterOptions: {
-//         filterAlgorithm: filterAlgorithm
-//       }
-//     })
-
-//     $table.bootstrapTable('filterBy', {
-//         gsx$categoria.$t:filterAlgorithm
-//     })
-//   })
