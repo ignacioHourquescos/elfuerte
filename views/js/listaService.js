2db4785e88e6fc218f44c96b7ab5fd68f9a0f91d
jimancio <ihourquescos@gmail.com>
2020-11-11 18:18:26 -0300

    algunos cambios

--- views/js/listaService.js
+++ views/js/listaService.js
@@ -7,32 +7,44 @@ document.getElementById("select").addEventListener("click", function() {
 });
 
 
- function obtenerListaEspecifica () {
- 	$.getJSON('https://spreadsheets.google.com/feeds/list/1oEIH4yWEVMNmJOENvGau2Le9hGssFQss6RiFp7_EzFU/1/public/values?alt=json', function (data) {       
-        var categoria_filtrada=document.getElementById("select").value;
-        var dataFiltrada=data.feed.entry;
-        console.log(dataFiltrada);
-        var dataFiltrada2;
-        if(categoria_filtrada=="Todas las Categorias"){dataFiltrada2=dataFiltrada;}
-        else{dataFiltrada2 = dataFiltrada.filter(function(producto) {
-                return producto.gsx$categoria.$t == categoria_filtrada;
-            });
+function obtenerListaEspecifica () {
+  $.getJSON('https://spreadsheets.google.com/feeds/list/1oEIH4yWEVMNmJOENvGau2Le9hGssFQss6RiFp7_EzFU/1/public/values?alt=json', function (data) {       
+       var categoria_filtrada=document.getElementById("select").value;
+       var dataFiltrada=data.feed.entry;
+       var array=[];
+       for (var i=0;i<dataFiltrada.length;i++){
+        array.push({
+         "codi":dataFiltrada[i].gsx$codigo.$t,
+         "desc":dataFiltrada[i].gsx$descripcion.$t,
+         "pres":dataFiltrada[i].gsx$presentacion.$t,
+         "cate":dataFiltrada[i].gsx$categoria.$t,
+         "prec":dataFiltrada[i].gsx$precio.$t,
+         "orig":dataFiltrada[i].gsx$origen.$t,
+        "desc2":dataFiltrada[i].gsx$desc2.$t,
+         });  
+       }
+      
+       if(categoria_filtrada=="Todas las Categorias"){
+          array=array;
         }
-        $('table').bootstrapTable('destroy');
-        $('table').bootstrapTable({ 
-            data: dataFiltrada2,
-        }); 
+       else{
+         array = array.filter(function(producto) {
+               return producto.cate == categoria_filtrada;
+           });
+       }
+       $('table').bootstrapTable('destroy');
+       $('table').bootstrapTable({ 
+           data: array,
+       }); 
 
-        // $('table').bootstrapTable('destroy');
-        // $('table').bootstrapTable({ 
-        //     data: data,
-        // }); 
-        
-    });
-    
+       // $('table').bootstrapTable('destroy');
+       // $('table').bootstrapTable({ 
+       //     data: data,
+       // }); 
+       
+   });
+   
 }
-
-
 
 
 
@@ -50,7 +62,7 @@ function operateFormatter(value, row, index) {
 
   window.operateEvents = {
     'click .like': function (e, value, row, index) {
-      alert( "Descirpcion Adicional: " + JSON.stringify(row.gsx$categoria.$t) + " de " + JSON.stringify(row.gsx$origen.$t) )
+      swal(JSON.stringify(row.desc2) )
     },
     'click .remove': function (e, value, row, index) {
       $table.bootstrapTable('remove', {
@@ -71,4 +83,9 @@ if (window.matchMedia("(max-width: 600px)").matches) {
 	console.log("mas de 400px");
 	document.getElementById("myTable").removeAttribute("data-show-header");
 }
+
+
+
+////////////////SCROLLER A LA PALABRA
+
 
