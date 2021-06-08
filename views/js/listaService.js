bf26d9b231ed02d0bd91f6379dcaf6f211a85d57
jimancio <ihourquescos@gmail.com>
2021-06-08 00:14:28 -0300

    cambios junio

--- views/js/listaService.js
+++ views/js/listaService.js
@@ -1,96 +1,101 @@
-
-// function cambiaridioma(){
-//  getElementsByClassName("search-input").placeholder="Buscar";
-// }
-
-
 document.getElementById("select").addEventListener("click", function() {
-
     obtenerListaEspecifica();
 });
 
+function obtenerListaEspecifica() {
+   $.getJSON('https://spreadsheets.google.com/feeds/list/1JIm_6mWe8rJ3yTDX88LMUhKQkzGn9xvUpcMs15fJNSc/1/public/values?alt=json', function (data) {
+      var categoria_filtrada = document.getElementById("select").value;
+      var data = data.feed.entry;
+      var array = [];
 
-function obtenerListaEspecifica () {
-  $.getJSON('https://spreadsheets.google.com/feeds/list/1JIm_6mWe8rJ3yTDX88LMUhKQkzGn9xvUpcMs15fJNSc/1/public/values?alt=json', function (data) {       
-       var categoria_filtrada=document.getElementById("select").value;
-       var data=data.feed.entry;
-       var array=[];
+      for (var i = 0; i < data.length; i++) {
+         var lastUpdate = convertirFecha(data[i].gsx$fecha.$t);
 
-       for (var i=0;i<data.length;i++){
-        var lastUpdate = convertirFecha(data[i].gsx$fecha.$t);
-        array.push({
-         "codi":data[i].gsx$codigo.$t,
-         "desc":data[i].gsx$descripcion.$t,
-         "pres":data[i].gsx$presentacion.$t,
-         "cate":data[i].gsx$categoria.$t,
-         "prec":'$'+parseInt(data[i].gsx$precio.$t),
-         "orig":data[i].gsx$origen.$t,
-         "desc2":data[i].gsx$desc2.$t,
-            "fecha":lastUpdate,
-         });  
-       }
-       console.log(array)
-       if(categoria_filtrada=="Todas las Categorias"){
-          array=array;
-        }
-       else{
-         array = array.filter(function(producto) {
-               return producto.cate == categoria_filtrada;
-           });
-       }
-       $('table').bootstrapTable('destroy');
-       $('table').bootstrapTable({ 
-           data: array,
-       }); 
+         if (lastUpdate ==false){
+         array.push({
+            "codi": data[i].gsx$codigo.$t,
+            "desc": data[i].gsx$descripcion.$t,
+            "pres": data[i].gsx$presentacion.$t,
+            "cate": data[i].gsx$categoria.$t,
+            "prec": '$' + parseInt(data[i].gsx$precio.$t),
+            "orig": data[i].gsx$origen.$t,
+            "desc2": data[i].gsx$desc2.$t,
+            "fecha": lastUpdate,
+         });
+         }else if (window.matchMedia("(max-width: 600px)").matches) {
+            array.push({
+               "codi": data[i].gsx$codigo.$t,
+               "desc": data[i].gsx$descripcion.$t + "<span style='margin:1rem;color:red; border:1px solid red;background-color:#ec9c9cbf; border-radius:1000px; padding:0 0.5rem;'>modificado</span>",
+               "pres": data[i].gsx$presentacion.$t,
+               "cate": data[i].gsx$categoria.$t,
+               "prec": '$' + parseInt(data[i].gsx$precio.$t),
+               "orig": data[i].gsx$origen.$t,
+               "desc2": data[i].gsx$desc2.$t,
+               "fecha": lastUpdate,
+            });
+         }else{
+            array.push({
+               "codi": data[i].gsx$codigo.$t,
+               "desc": data[i].gsx$descripcion.$t + "&nbsp<span style='color:red; border:1px solid red;background-color:#ec9c9cbf; border-radius:100px; padding:0 0.5rem;'>Modificado esta semana</span>",
+               "pres": data[i].gsx$presentacion.$t,
+               "cate": data[i].gsx$categoria.$t,
+               "prec": '$' + parseInt(data[i].gsx$precio.$t),
+               "orig": data[i].gsx$origen.$t,
+               "desc2": data[i].gsx$desc2.$t,
+               "fecha": lastUpdate,
+            });
+         }
+      }
 
-       // $('table').bootstrapTable('destroy');
-       // $('table').bootstrapTable({ 
-       //     data: data,
-       // }); 
-       
+      if (categoria_filtrada == "Todas las Categorias") {
+         array = array;
+
+      } else {
+         array = array.filter(function (producto) {
+            return producto.cate == categoria_filtrada;
+         });
+      }
+      $('table').bootstrapTable('destroy');
+      $('table').bootstrapTable({
+         data: array,
+      });
    });
-   
 }
 
 
 
 
 function operateFormatter(value, row, index) {
-    if(row.desc2==""){
-        return [
-            '</a>  ',
-      
-            '</a>'
-          ].join('')
-    }else{
-        return [
-     
-            '</a>  ',
-            '<a class="like" href="javascript:void(0)" title="Remove">',
-            '<i class="fa fa-info-circle" style="color:grey;"></i>',
-      
-            '</a>'
-          ].join('')
-    }
-
-
-
-  }
+   if (row.desc2 == "") {
+      return [
+         '</a>  ',
+         '<p style="color:#e8e8e8;">Sin detalle</p>',
+         '</a>'
+      ].join('')
+   } else {
+      return [
+         '</a>  ',
+         '<a class="like" href="javascript:void(0)" title="Remove">',
+         `<p style="color:grey;">${row.desc2.substring(0,20)}...</p>`,
+         '</a>'
+      ].join('')
+   }
+}
 
   window.operateEvents = {
-    'click .like': function (e, value, row, index) {
-        if(row.desc2==""){
-            swal(JSON.stringify(row.desc) )
-        }else{
-        swal(JSON.stringify(row.desc2) )
+     'click .like': function (e, value, row, index) {
+        if (row.desc2 == "") {
+           swal(JSON.stringify(row.desc))
+        } else {
+           swal(JSON.stringify(row.desc2))
         }
-    },
-    'click .remove': function (e, value, row, index) {
-      $table.bootstrapTable('remove', {
-        field: 'id',
-        values: [row.id]
-      })
-    }
+     },
+     'click .remove': function (e, value, row, index) {
+        $table.bootstrapTable('remove', {
+           field: 'id',
+           values: [row.id]
+        })
+     }
   }
 
  obtenerListaEspecifica();
@@ -98,7 +103,6 @@ function operateFormatter(value, row, index) {
 //escondo table header cuando es un celular, y si es en PC lo muestro
 if (window.matchMedia("(max-width: 600px)").matches) {
   document.getElementById("myTable").removeAttribute("data-show-header");
-	
 } else {
 	document.getElementById("myTable").removeAttribute("data-show-header");
 }
@@ -107,15 +111,14 @@ if (window.matchMedia("(max-width: 600px)").matches) {
 
 //////////////// HANDLER DE FECHAS
 const convertirFecha = (fecha) => {
-  var dateParts = fecha.split("/");
-  var dateObject = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]); 
-  const diffDays = Math.ceil(Math.abs(new Date() - dateObject) / (1000 * 60 * 60 * 24)); 
-  if (diffDays<7)
-    // return ['<i class="fa fa-circle" style="color:red">Lista</i>']
-      return ['<h4 style="color:red;">Modificado esta semana</h4>']
-  else
-    return ("");
+   var dateParts = fecha.split("/");
+   var dateObject = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]);
+   const diffDays = Math.ceil(Math.abs(new Date() - dateObject) / (1000 * 60 * 60 * 24));
+   if (diffDays < 7) {
+   
+      return true
+   } else
+      return (false);
 }
-
 
 
